2cbdc6f2a51259e5be28c69a1eaaa9285ce58833
Lucas Orlikoski <lucasorlikoski099@gmail.com>
2024-06-23 17:00:45 -0300

    Adicionado chat

--- client.py
+++ client.py
@@ -3,7 +3,6 @@ import threading
 import socket
 import os
 from datetime import datetime
-import random
 
 class ClientGUI:
     def __init__(self, master, client_id, port):
@@ -11,13 +10,13 @@ class ClientGUI:
         self.client_id = client_id
         self.client_port = port
 
-        # Configurações do cliente
         self.SERVER_HOST = '127.0.0.1'
         self.SERVER_PORT = 9999
         self.FILE_REQUEST = ''
         self.FILE_NAME = f'arquivo_{client_id}'
         self.client_socket = None
-        self.thread_receive = None  # Inicializa como None
+        self.thread_receive = None
+        self.running = False
 
         self.frame = tk.Frame(self.master)
         self.frame.pack(pady=10)
@@ -32,46 +31,69 @@ class ClientGUI:
         self.porta_label = tk.Label(self.frame, text=str(self.client_port))
         self.porta_label.grid(row=0, column=3, padx=5, pady=5)
 
-        self.botao_enviar = tk.Button(self.frame, text=f"Enviar Request Cliente {self.client_id}", command=self.send_request)
-        self.botao_enviar.grid(row=0, column=4, padx=5, pady=5)
+        self.connect_button = tk.Button(self.frame, text="Conectar ao Servidor", command=self.toggle_connection)
+        self.connect_button.grid(row=0, column=4, padx=5, pady=5)
+
+        self.botao_enviar = tk.Button(self.frame, text=f"Enviar Request Cliente {self.client_id}", command=self.send_request, state=tk.DISABLED)
+        self.botao_enviar.grid(row=1, column=4, padx=5, pady=5)
 
         self.size_label = tk.Label(self.frame, text="Tamanho da resposta: ")
-        self.size_label.grid(row=1, columnspan=5, padx=5, pady=5)
+        self.size_label.grid(row=2, columnspan=5, padx=5, pady=5)
 
         self.status_label = tk.Label(self.frame, text="Status: ")
-        self.status_label.grid(row=2, columnspan=5, padx=5, pady=5)
+        self.status_label.grid(row=3, columnspan=5, padx=5, pady=5)
 
         self.progress_label = tk.Label(self.frame, text="Progresso: 0%")
-        self.progress_label.grid(row=3, columnspan=5, padx=5, pady=5)
+        self.progress_label.grid(row=4, columnspan=5, padx=5, pady=5)
 
-        self.reset_button = tk.Button(self.frame, text="Resetar Cliente", command=self.reset_client)
-        self.reset_button.grid(row=4, columnspan=5, padx=5, pady=5)
+        self.chat_label = tk.Label(self.frame, text="Mensagens do servidor:")
+        self.chat_label.grid(row=5, columnspan=5, padx=5, pady=5)
 
-    def send_request(self):
-        self.FILE_REQUEST = f'OBTER filesToSend/{self.entry_arquivo.get()}'
+        self.chat_text = tk.Text(self.frame, height=10, width=50)
+        self.chat_text.grid(row=0, column=5, rowspan=6, padx=5, pady=5, sticky='ns')
 
+    def toggle_connection(self):
+        if self.client_socket:
+            self.reset_client()
+            self.botao_enviar.config(state=tk.DISABLED)
+        else:
+            self.connect_to_server()
+
+    def connect_to_server(self):
         try:
             if self.client_socket is None:
                 self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 self.client_socket.bind(('', self.client_port))
                 self.client_socket.connect((self.SERVER_HOST, self.SERVER_PORT))
+                self.running = True
 
             if self.thread_receive is None or not self.thread_receive.is_alive():
-                # Inicia a thread de recebimento apenas se não estiver ativa
                 self.thread_receive = threading.Thread(target=self.receive_response)
                 self.thread_receive.daemon = True
                 self.thread_receive.start()
 
-            self.client_socket.send(self.FILE_REQUEST.encode())
-            print(f'Requisição enviada para o servidor pelo Cliente {self.client_id}.')
+            self.status_label.configure(text="Status: Conectado ao servidor")
+            self.connect_button.configure(text="Desconectar do Servidor")
+            print(f'Cliente {self.client_id} conectado ao servidor.')
+            self.botao_enviar.config(state=tk.NORMAL)
+
         except ConnectionRefusedError:
             self.status_label.configure(text="Status: Erro - Servidor recusou a conexão ou está desligado")
             print("Erro: Servidor recusou a conexão ou está desligado")
-        
+
+    def send_request(self):
+        self.FILE_REQUEST = f'OBTER filesToSend/{self.entry_arquivo.get()}'
+        try:
+            if self.client_socket:
+                self.client_socket.send(self.FILE_REQUEST.encode())
+                print(f'Requisição enviada para o servidor pelo Cliente {self.client_id}.')
+        except Exception as e:
+            self.status_label.configure(text=f"Status: Erro ao enviar requisição - {e}")
+            print(f'Erro ao enviar requisição: {e}')
+
     def receive_response(self):
-        nome_arquivo = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}_{self.client_id}_{self.FILE_NAME}"
-        extensao = self.FILE_REQUEST.split(".")[-1]
-        nome_arquivo = f"{nome_arquivo}.{extensao}"
+        extensao = self.entry_arquivo.get().split(".")[-1] if "." in self.entry_arquivo.get() else "bin"
+        nome_arquivo = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}_{self.client_id}_{self.FILE_NAME}.{extensao}"
 
         if not os.path.exists("filesReceived"):
             os.makedirs("filesReceived")
@@ -81,31 +103,35 @@ class ClientGUI:
         tamanho_arquivo = 0
 
         try:
-            pacote_inicial = self.client_socket.recv(8192)
-            resposta = pacote_inicial.decode()
-
-            if resposta.startswith("Erro"):
-                self.status_label.configure(text=f"Status: {resposta}")
-                print(f'Erro recebido do servidor: {resposta}')
-                return
-            elif resposta.startswith("TAMANHO"):
-                tamanho_arquivo = int(resposta.split(" ")[1])
+            while self.running:
                 pacote_inicial = self.client_socket.recv(8192)
+                if not pacote_inicial:
+                    break
+                resposta = pacote_inicial.decode(errors='ignore')
 
-            with open(caminho_arquivo, 'wb') as file:
-                file.write(pacote_inicial)
-                num_bytes += len(pacote_inicial)
-                self.update_progress(num_bytes, tamanho_arquivo)
-
-                while True:
-                    pacote = self.client_socket.recv(8192)
-                    if not pacote:
-                        break
-                    file.write(pacote)
-                    num_bytes += len(pacote)
-                    #gambiarra pra atualizar só as vezes o progresso 
-                    if num_bytes % 10 == 0:
+                if resposta.startswith("SAIR"):
+                    self.reset_client()
+                    break
+                elif resposta.startswith("TAMANHO"):
+                    tamanho_arquivo = int(resposta.split(" ")[1])
+                    continue  # Continue recebendo os dados do arquivo
+                elif resposta.startswith("CHAT"):
+                    self.chat_text.insert(tk.END, f'{resposta.replace("CHAT ", "Aviso do servidor: ")}\n')
+                    continue  # Continue recebendo mensagens de chat
+                else:
+                    # Primeiro pacote não deve conter "TAMANHO", então reabra o arquivo para escrita
+                    with open(caminho_arquivo, 'ab') as file:
+                        file.write(pacote_inicial)
+                        num_bytes += len(pacote_inicial)
                         self.update_progress(num_bytes, tamanho_arquivo)
+
+                        while num_bytes < tamanho_arquivo:
+                            pacote = self.client_socket.recv(8192)
+                            if not pacote:
+                                break
+                            file.write(pacote)
+                            num_bytes += len(pacote)
+                            self.update_progress(num_bytes, tamanho_arquivo)
 
             self.update_progress(num_bytes, tamanho_arquivo)
             self.size_label.configure(text=f"Tamanho da resposta: {num_bytes / 1024:.2f} KB")
@@ -113,33 +139,37 @@ class ClientGUI:
             print(f'Arquivo recebido salvo em: {caminho_arquivo}')
 
         except Exception as e:
-            self.status_label.configure(text=f"Status: Erro - {e}")
-            print(f'Erro ao receber o arquivo: {e}')
+            if self.running:
+                self.status_label.configure(text=f"Status: Erro - {e}")
+                print(f'Erro ao receber o arquivo: {e}')
 
         finally:
-            if self.client_socket:
-                self.client_socket.close()
-                self.client_socket = None
+            self.reset_client()
+
+    def get_file_extension(self, file_name):
+        _, ext = os.path.splitext(file_name)
+        return ext[1:] if ext else "bin"
 
     def update_progress(self, bytes_received, total_bytes):
         percentage = (bytes_received / total_bytes) * 100
         self.progress_label.configure(text=f"Progresso: {percentage:.2f}%")
 
     def reset_client(self):
-        self.entry_arquivo.delete(0, tk.END)
+        self.botao_enviar.config(state=tk.DISABLED)
         self.size_label.configure(text="Tamanho da resposta: ")
         self.status_label.configure(text="Status: ")
         self.progress_label.configure(text="Progresso: 0%")
+        self.chat_text.delete(1.0, tk.END)
         if self.client_socket:
             self.client_socket.close()
             self.client_socket = None
-        if self.thread_receive and self.thread_receive.is_alive():
-            self.thread_receive.join()  # Espera até que a thread termine
+        self.running = False
+        self.connect_button.configure(text="Conectar ao Servidor")
 
 if __name__ == "__main__":
     janela = tk.Tk()
     janela.title("Gerenciador de Clientes")
 
-    clients = [ClientGUI(janela, i, 8000 + i) for i in range(1, 4)]
+    clients = [ClientGUI(janela, i, 7000 + i) for i in range(1, 4)]
 
     janela.mainloop()
--- server.py
+++ server.py
@@ -7,57 +7,82 @@ HOST = '127.0.0.1'
 PORT = 9999
 BUFFER_SIZE = 8192
 
+clients = []
+lock = threading.Lock()
+
 class OrlikoskiProtocol:
     def __init__(self):
         pass
 
     def processar_requisicao(self, mensagem):
-        if not mensagem.startswith("OBTER "):
-            return "Erro: Formato de requisição inválido."
-        arquivo_requisitado = mensagem.split(" ")[1]
-        return arquivo_requisitado
+        if mensagem.startswith("SAIR"):
+            return "SAIR"
+        elif mensagem.startswith("OBTER "):
+            arquivo_requisitado = mensagem.split(" ")[1]
+            return f'OBTER {arquivo_requisitado}'
 
-    def processar_resposta_arquivo(self, dados_arquivo):
-        if not dados_arquivo:
-            return "Erro: Dados do arquivo não recebidos."
-        return dados_arquivo
-
-    def processar_confirmacao(self, mensagem):
-        return mensagem
-
-    def processar_erro(self, mensagem):
-        return mensagem
-
-def handle_client(client_socket):
-    protocolo = OrlikoskiProtocol()
-    try:
-        request = client_socket.recv(1024).decode()
-        print(f'Requisição recebida: {request}')
-        
-        arquivo_requisitado = protocolo.processar_requisicao(request)
-        
-        if arquivo_requisitado.startswith("Erro"):
-            client_socket.send(arquivo_requisitado.encode())
-            print('Mensagem de erro enviada para o cliente.')
-        else:
+    def enviar_arquivo(self, client_socket, arquivo_requisitado):
+        try:
             if os.path.exists(arquivo_requisitado):
                 tamanho_arquivo = os.path.getsize(arquivo_requisitado)
                 client_socket.send(f"TAMANHO {tamanho_arquivo}".encode())
+                
                 with open(arquivo_requisitado, 'rb') as file:
                     while True:
                         dados = file.read(BUFFER_SIZE)
                         if not dados:
                             break
                         client_socket.send(dados)
-                    print(f'Arquivo {arquivo_requisitado} enviado para o cliente.')
+                print(f'Arquivo {arquivo_requisitado} enviado para o cliente.')
             else:
                 mensagem_erro = 'Erro: Arquivo não encontrado'
                 client_socket.send(mensagem_erro.encode())
                 print('Mensagem de erro enviada para o cliente.')
+                
+        except Exception as e:
+            print(f'Erro ao enviar arquivo: {e}')
+
+def handle_client(client_socket):
+    protocolo = OrlikoskiProtocol()
+    clients.append(client_socket)
+    try:
+        while True:
+            request = client_socket.recv(1024).decode()
+            if not request:
+                break
+            print(f'Requisição recebida: {request}')
+            
+            resposta = protocolo.processar_requisicao(request)
+
+            if resposta.startswith("SAIR"):
+                break
+            elif resposta.startswith("OBTER"):
+                arquivo_requisitado = resposta.split(" ")[1]
+                protocolo.enviar_arquivo(client_socket, arquivo_requisitado)
+            else:
+                broadcast_message(resposta)
+
+    except ConnectionResetError:
+        print(f'Conexão fechada pelo cliente de forma inesperada.')
     except Exception as e:
         print(f'Erro ao processar requisição: {e}')
+
     finally:
+        with lock:
+            clients.remove(client_socket)
         client_socket.close()
+        print(f'Cliente desconectado.')
+
+def broadcast_message(message):
+    print(message, len(clients))
+
+    for client in clients[:]:
+        try:
+            client.send(message.encode())
+        except Exception as e:
+            print(e)
+            with lock:
+                clients.remove(client)
 
 def main():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -65,11 +90,46 @@ def main():
     server_socket.listen(5)
     print('Servidor TCP iniciado.')
 
+    threading.Thread(target=console_input_thread).start()
+
     while True:
         client_socket, addr = server_socket.accept()
         print(f'Conexão aceita de {addr}')
         client_handler = threading.Thread(target=handle_client, args=(client_socket,))
         client_handler.start()
+
+def console_input_thread():
+    while True:
+        command = input()
+
+        if command.startswith("SAIR"):
+            try:
+                if command == "SAIR":
+                    # Desconectar todos os clientes
+                    with lock:
+                        for client in clients:
+                            client.send("SAIR".encode())
+                            client.close()
+                        clients.clear()
+                        print("Todos os clientes desconectados.")
+                elif command.startswith("SAIR "):
+                    # Desconectar cliente específico pelo índice
+                    index = int(command.split()[1])
+                    if 0 <= index < len(clients):
+                        client = clients[index]
+                        with lock:
+                            clients.remove(client)
+                            client.send("SAIR".encode())
+                            client.close()
+                            print(f"Cliente {index} desconectado.")
+                    else:
+                        print("Índice inválido.")
+                else:
+                    print("Comando inválido.")
+            except ValueError:
+                print("Comando inválido.")
+        else:
+            broadcast_message("CHAT " + command)
 
 if __name__ == "__main__":
     main()
